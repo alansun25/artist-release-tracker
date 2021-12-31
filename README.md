16576524959b4346637fd78ac6fba314edb5f944
Alan Sun <mralansun@gmail.com>
2021-12-30 23:22:33 -0500

    Added ability to create new playlist

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # artist-release-tracker
 
-A Python script that will automatically add your favorite artists' new releases into a playlist on Spotify. Currently a work in progress.
+**Work in progress.**
+
+Tools used: Python, Spotify Web API, spotipy
--- main.py
+++ main.py
@@ -1,16 +1,34 @@
+import os
+import sys
+from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+def main():
+  load_dotenv()
 
-id = os.environ.get('SPOTIPY_CLIENT_ID')
-secret = os.environ.get('SPOTIPY_CLIENT_SECRET')
-ruri = os.environ.get('SPOTIPY_REDIRECT_URI')
-scope = os.environ.get('SPOTIPY_SCOPE')
+  id = os.environ.get('SPOTIPY_CLIENT_ID')
+  secret = os.environ.get('SPOTIPY_CLIENT_SECRET')
+  ruri = os.environ.get('SPOTIPY_REDIRECT_URI')
+  scope = os.environ.get('SPOTIPY_SCOPE')
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=id,
-                                               client_secret=secret,
-                                               redirect_uri=ruri,
-                                               scope=scope))
+  sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=id,
+                                                client_secret=secret,
+                                                redirect_uri=ruri,
+                                                scope=scope))
+  
+  user_id = sp.me()['id']
+  user_playlists = sp.user_playlists(user_id)
+  
+  playlist_name = input("Enter the name of your new playlist:\n")
+  
+  for playlist in user_playlists:
+    if playlist['name'] == playlist_name:
+      print('Playlist named', playlist_name, 'already exists.')
+      sys.exit()
+  
+  # playlist = sp.user_playlist_create(user_id, "Test", False, False, "Testing my Python script")
+  # playlist_id = playlist['id']
+
+if __name__ == "__main__":
+    main()
