ff30af966da7c01d6abfbc9983db0437fe3bc463
Alan Sun <mralansun@gmail.com>
2022-04-05 22:58:56 +0200

    Added search selection and multiple file creation

--- main.py
+++ main.py
@@ -44,36 +44,27 @@ def main():
   user_playlists = sp.user_playlists(user_id)
   
   # Create a new playlist if a playlist with the same name doesn't already exist.
-  # If the playlist already exists, ask the user if they would like to use that
-  # existing playlist.
-  playlist_name = input("Enter the name of your new playlist or an existing playlist (case sensitive):\n")
- 
+  # If the playlist already exists, use that existing playlist.
+  playlist_name = input('Enter the name of your new playlist or an existing playlist (case sensitive):\n')
   if playlist_exists(playlist_name, user_playlists):
-    # print('Playlist named', '"' + playlist_name + '"', 'already exists.')
-    # prompt = input('Would you like to use the existing playlist? (y/n): ')
-    # prompt = prompt.lower()
-    
-    # if prompt == 'y':
     for playlist in user_playlists['items']:
       if playlist['name'] == playlist_name:
         playlist_id = playlist['id']
-    # else:
-    #   print("Please re-run the script and try again with a different playlist name.")
-    #   sys.exit()
   else:
-    playlist_desc = input("Enter a description for your new playlist:\n")
+    playlist_desc = input('Enter a description for your new playlist:\n')
     playlist = sp.user_playlist_create(
         user_id, playlist_name, False, False, playlist_desc)
     playlist_id = playlist['id']
   
   # Create file to store artists user wants to track (data persistence).
-  artists = open('./artists.txt', 'r+')
+  file = f'./{playlist_name.replace(" ", "-")}.txt'
+  artists = open(file, 'w+')
   existing_artists = artists.readlines()
   print(existing_artists)
   
   while True:
     # Prompt the user to provide artists for which they would like to track new releases.
-    artist = input('Name of an artist you would like to track (type "exit" when done):\n')
+    artist = input('Name of an artist you would like to track (type \'exit\' when done):\n')
     
     if artist == 'exit':
       break
@@ -82,23 +73,38 @@ def main():
     artist = artist.lower().strip()
     
     # Check that the artist is not already in the user's list of tracked artists.
-    if artist not in existing_artists:
+    if artist + '\n' not in existing_artists:
       artists.write(artist + '\n')
   
   artists.close()
   
   # Replace items currently in playlist with the most recent releases from each artist.
   new_tracks = []
-  with open("artists.txt", "r") as tracked_artists:
+  with open(file, 'r') as tracked_artists:
     for tracked_artist in tracked_artists:
       query_results = sp.search(tracked_artist, 10, 0, 'artist', user['country'])['artists']['items']
       
-      # This assumes the user is looking for the artist with the given name that is the
-      # most popular and has the most followers.
+      # Sort query results by artist popularity first and then total follower count.
       query_results.sort(key = lambda i: (i['popularity'], i['followers']['total']), reverse = True)
       
+      # Ask user to select from the most popular artists retrieved from the query (either the
+      # top 5 most popular artists from the query or the number of results, whichever is fewer).
+      end = min(5, len(query_results))
+      for i in range(0, end):
+        genres = ', '.join(query_results[i]['genres'])
+        if not len(genres):
+          # No genre listed for the particular artist.
+          genres = 'n/a'
+        print(f'({i}) Artist: {query_results[i]["name"]}, Genres: {genres}\n')
+        
+      artist_idx = input(f'From the choices above, enter the index of the artist you were looking for (0 thru {end - 1}): ')
+      artist_idx = int(artist_idx)
+      while artist_idx > end - 1 or artist_idx < 0:
+        artist_idx = input(f'Please enter a value between 0 and {end - 1}: ')
+        artist_idx = int(artist_idx)
+      
       # Get artist most recent release (can be a single, album, or feature).
-      artist_id = query_results[0]['id']
+      artist_id = query_results[artist_idx]['id']
       artist_albums = sp.artist_albums(artist_id, 'album,single,appears_on', user['country'], 50)['items']
       artist_albums.sort(key=lambda i: datetime.strptime(i['release_date'], '%Y-%m-%d'), reverse = True)
       
@@ -130,5 +136,5 @@ def main():
   # Close the file.
   tracked_artists.close()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
