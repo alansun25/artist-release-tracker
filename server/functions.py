d11a3f1865ce35bd3cd17f60438ef8e0608ae9c1
Alan Sun <mralansun@gmail.com>
2022-12-26 15:03:49 -0500

    connected svelte client with flask server

--- server/functions.py
+++ server/functions.py
@@ -1,45 +1,38 @@
+import jsonpickle
 import os
-import random as rand
-import spotipy
-import string
-from artist_radar import ArtistRadar
-from init.firebase import initialize_firebase_db
+from dotenv import load_dotenv, find_dotenv
 from spotipy.oauth2 import SpotifyOAuth
+
+load_dotenv()
 
 CLIENT_ID = os.environ.get("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.environ.get("SPOTIPY_CLIENT_SECRET")
 R_URI = os.environ.get("SPOTIPY_REDIRECT_URI")
+STATE = str(os.urandom(15))
 SCOPE = os.environ.get("SPOTIPY_SCOPE")
 
-AUTH = SpotifyOAuth(CLIENT_ID, CLIENT_SECRET, R_URI, scope=SCOPE)
-TOKEN_DATA = {}
-
-def create_secret_key(size):
-    return ''.join(rand.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(size))
+AUTH = SpotifyOAuth(CLIENT_ID, CLIENT_SECRET, R_URI, STATE, SCOPE)
 
 def get_auth_url():
     return AUTH.get_authorize_url()
 
-def get_token_data(code):
-    global TOKEN_DATA
+def get_token_data(code, state):    
+    if state != AUTH.state:
+        # TODO: Handle state validation
+        pass
     
-    TOKEN_DATA = AUTH.get_cached_token()
-    if not TOKEN_DATA:
-        TOKEN_DATA = AUTH.get_access_token(code)
+    token = AUTH.get_cached_token()
+    if not token:
+        token = AUTH.get_access_token(code)
     
-    initialize_spotify_client()
+    return token
 
-def refresh_token():
+def check_refresh_token(token):
     # Note: Spotify access tokens have a lifetime of 1 hour
-    global TOKEN_DATA
+    if AUTH.is_token_expired(token):
+        token = AUTH.refresh_access_token(token['refresh_token'])
     
-    if AUTH.is_token_expired(TOKEN_DATA):
-        TOKEN_DATA = AUTH.refresh_access_token(TOKEN_DATA['refresh_token'])
-        initialize_spotify_client()
+    return token
 
-def initialize_spotify_client():
-    global sp    
-    sp = spotipy.Spotify(TOKEN_DATA['access_token'])
-
-def create_artist_radar():
-    return ArtistRadar(sp, initialize_firebase_db())
+def to_json(obj):
+    return jsonpickle.encode(obj)
--- server/routes.py
+++ server/routes.py
@@ -1,15 +1,19 @@
-from flask import Flask, redirect, request, session
-from functions import create_secret_key, get_auth_url, get_token_data
+import os
+import spotipy
+import time
+from flask import Flask, redirect, request, send_from_directory, session
+from functions import get_auth_url, get_token_data, to_json
 
 app = Flask(__name__)
-app.config.update(SECRET_KEY=create_secret_key(15))
+app.config.update(SECRET_KEY=os.urandom(24))
 
 @app.route("/")
-def home():
-    if 'token_received' not in session:
-        return redirect('/authorize')
+def root():
+    # if 'access_token' not in session:
+    #     return redirect('/authorize')
 
-    return "<p>Hello, World!</p>"    
+    # return "<p>Hello, World!</p>"    
+    return send_from_directory('../client/dist', 'index.html')
 
 @app.route('/authorize')
 def authorize():
@@ -19,8 +23,21 @@ def authorize():
 @app.route('/callback')
 def callback():
     code = request.args.get('code')
-    get_token_data(code)
-    session['token_received'] = True
+    state = request.args.get('state')
+    token_data = get_token_data(code, state)
+    
+    # TODO: Handle when token data not returned due to state conflict
+    session['access_token'] = token_data['access_token']
+    session['refresh_token'] = token_data['refresh_token']
+    session['token_expiration'] = time.time() + token_data['expires_in']
+    
+    session['spotify'] = to_json(spotipy.Spotify(session['access_token']))
     
     return redirect('/')
-    
+
+@app.route("/<path:path>")
+def assets(path):
+    return send_from_directory('../client/dist', path)
+
+if __name__ == "__main__":
+    app.run(debug=True)
